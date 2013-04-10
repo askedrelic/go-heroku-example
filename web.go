9846688fd1d49318460c794661cf0ea2cb4395c6
Matt Behrens <askedrelic@gmail.com>
2013-04-09 21:38:32 -0700

    sloth it up

--- web.go
+++ web.go
@@ -3,11 +3,30 @@ package main
 import (
     "os"
     "github.com/hoisie/web"
+    "math/rand"
+    "time"
+    "fmt"
 )
 
-func hello(val string) string { return "hello " + val }
+var sloths = []string{
+    "http://cuteoverload.files.wordpress.com/2012/03/6852916110_3a06e758d9_z.jpg",
+    "http://static.environmentalgraffiti.com/sites/default/files/images/Sloth2.img_assist_custom-600x450.jpg",
+    "http://files.sharenator.com/sloth_3_Baby_Sloths-s750x500-301461.jpg",
+    "http://www.veganreader.com/images/pictureofsloth.gif",
+    "http://www.jakobdezwart.com/wp-content/uploads/2012/11/jakobdezwart-bestof-sloths-9517.jpg",
+    "http://media.smithsonianmag.com/images/Three-toed-sloth-Panama-631.jpg",
+}
+
+func hello(val string) string {
+    return fmt.Sprintf(`
+    <!DOCTYPE html><html><body>
+    <img src="%s" />
+    </body></html>
+`, sloths[rand.Intn(len(sloths))])
+}
 
 func main() {
+    rand.Seed(time.Now().UnixNano())
     port := os.Getenv("PORT")
     if port == "" { port = "9999" }
     web.Get("/(.*)", hello)
